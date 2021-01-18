30a45fb40ba116f8ad9064a58def33a40d7f69a3
ganariya <ganariya2525@gmail.com>
2021-01-18 10:02:21 +0900

    simple change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ganariya-neats',
-    version='0.1.2',
+    version='0.2.0',
     author='cesar.gomes, mirrorballu2, ganariya',
     author_email='nobody@nowhere.com',
     maintainer='CodeReclaimers, LLC, ganariya',
